f3d2dd67c2f961b75decf611e93ec82457326a31
Bakiyalakshmi Ramadoss <bakiyalakshmi.ramadoss1@ingka.ikea.com>
2024-02-08 21:09:13 +0100

    feat: second logger

--- test-project/libraries/package-a/index.js
+++ test-project/libraries/package-a/index.js
@@ -1,5 +1,6 @@
 
 export const helloMsg = () => {
     console.log('Hello from Package A!');
-    console.log('check the tagging')
+    console.log('check the tagging');
+    console.log('seconfd check the tagging');
   };
